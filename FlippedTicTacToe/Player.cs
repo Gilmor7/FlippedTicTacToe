acc29f5b85eee0b25a61751f72fc331f02166230
gilm <gilm@radware.com>
2023-05-09 00:49:39 +0300

    Add player class

---
+++ FlippedTicTacToe/Player.cs
@@ -0,0 +1,45 @@
+﻿using System;
+
+namespace FlippedTicTacToe
+{
+    class Player
+    {
+        private uint m_Score = 0;
+        private readonly eSymbols m_Symbol;
+        private readonly bool m_IsComputer;
+
+        public Player(eSymbols i_Symbol, bool i_IsComputer)
+        {
+            m_Symbol = i_Symbol;
+            m_IsComputer = i_IsComputer;
+        }
+
+        public uint Score
+        {
+            get
+            {
+                return m_Score;
+            }
+            set
+            {
+                m_Score = value;
+            }
+        }
+
+        public eSymbols Symbol
+        {
+            get
+            {
+                return m_Symbol;
+            }
+        }
+
+        public bool IsComputer
+        {
+            get
+            {
+                return m_IsComputer;
+            }
+        }
+    }
+}
