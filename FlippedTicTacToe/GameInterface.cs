a0c7bda775409bf8b76c83a880003d4ef85a836d
Eli Freid <86243882+E-Freid@users.noreply.github.com>
2023-05-10 20:47:03 +0300

    started interface again

--- FlippedTicTacToe/GameInterface.cs
+++ FlippedTicTacToe/GameInterface.cs
@@ -5,103 +5,91 @@ namespace FlippedTicTacToe
     public class GameInterface
     {
         private static GameEngine s_GameEngine = null;
-        private const bool k_WaitingForValidInput = true;
 
-        public static void StartGame()
+        private class UserInputValidator
         {
-            bool stillWantToPlay = true;
-
-            s_GameEngine = new GameEngine();
-            initGameEngineSettings();
-            while (stillWantToPlay)
+            public static bool ValidateYesOrNoInput(string i_UserInput)
             {
-                runGameLoop();
-                stillWantToPlay = askIfPlayerWantsToContinuePlaying();
+                string userInputUpperCase = i_UserInput.ToUpper();
+                return userInputUpperCase == "Y" || userInputUpperCase == "N";
+            }
+
+            public static bool ValidateBoardInput(string i_BoardSize)
+            {
+                int boardSize;
+                return int.TryParse(i_BoardSize, out boardSize);
+            }
+
+            public static bool ValidateUserMove(string i_RowMove, string i_ColMove)
+            {
+                int rowNumber, colNumber;
+
+                return int.TryParse(i_RowMove, out rowNumber) && int.TryParse(i_ColMove, out colNumber);
             }
         }
 
-        private static bool askIfPlayerWantsToContinuePlaying()
+        private class UserInputRequester
         {
-            string userInput;
-            bool userAnswer;
-
-            while (k_WaitingForValidInput)
+            public static string RequestBoardSizeInput()
             {
-                Console.WriteLine("Do you want to play again? Y/N");
-                userInput = Console.ReadLine();
-                switch (userInput.ToUpper())
-                {
-                    case "Y":
-                        userAnswer = true;
-                        break;
-                    case "N":
-                        userAnswer = false;
-                        break;
-                    default:
-                        Console.WriteLine("Invalid Input! try again!");
-                        continue;
-                }
+                Console.WriteLine("Enter desired board size: ");
+                return Console.ReadLine();
+            }
 
-                return userAnswer;
+            public static string RequestGameRestartInput()
+            {
+                Console.WriteLine("Do you want to play again? [Y/N]");
+                return Console.ReadLine();
+            }
+
+            public static string RequestIfSecondPlayerIsBot()
+            {
+                Console.WriteLine("Do you want to play aginst a real player? [Y/N]");
+                return Console.ReadLine();
+            }
+
+            public static (string, string) RequestUserMove()
+            {
+                string rowNumberString;
+                string colNumberString;
+
+                Console.WriteLine("Enter row number: ");
+                rowNumberString = Console.ReadLine();
+
+                Console.WriteLine("Enter col number: ");
+                colNumberString = Console.ReadLine();
+
+                return (rowNumberString, colNumberString);
             }
         }
 
-        private static void runGameLoop()
+        public static void Play()
         {
-            /*
-            while game is still running:
-            clear console
-            display board
-            if curr player is bot:
-                make random move
-            else:
-                ask for move
-                make move
-            check game status
-            
-             */
+            bool stillPlaying = true;
 
-            throw new NotImplementedException();
-        }
+            setInitialGameSettings();
 
-        private static void initGameEngineSettings()
-        {
-            setBoardFromUserInput();
-            setFirstPlayer();
-            setSecondPlayerFromUserInput();
-            // s_GameEngine.CurrentPlayer = FirstPlayer
-        }
-
-        private static void setFirstPlayer()
-        {
-            throw new NotImplementedException();          
-        }
-
-        private static void setSecondPlayerFromUserInput()
-        {
-            throw new NotImplementedException();
-        }
-
-        private static void setBoardFromUserInput()
-        {
-            const bool k_WaitingForValidInput = true;
-            int sizeOfBoard;
-
-            while (k_WaitingForValidInput)
+            while (stillPlaying)
             {
-                try
+                displayBoard();
+                makeCurrPlayerMove();
+                if(GameEngine.GameStatus == "OVER")
                 {
-                    sizeOfBoard = getSizeOfBoardFromUser();
-                    GameEngine.SetBoardSize(sizeOfBoard);
-                    return;
+                    displayGameOverScreen();
+                    bool userWantsToRestartGame = askIfUserWantsToRestartGame();    
+                    if (askIfUserWantsToResetGame())
+                    {
+                        GameEngine.ResetGame();
+                    }
+                    else
+                    {
+                        stillPlaying = false;
+                    }
                 }
-                catch(Exception e)
-                {
-                    Console.WriteLine(e.Message);
-                    Console.WriteLine("Please Try Again!");
-                    continue;
-                }
+
             }
+
+            Console.WriteLine("Thanks for playing! Goodbye :)");
         }
     }
 }
