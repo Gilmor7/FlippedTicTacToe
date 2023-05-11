0e983d2e631363df31031a111fe982eb61ef7efb
gilm <gilm@radware.com>
2023-05-11 16:58:40 +0300

    add interface assembly for the game and gameConsoleUtils class with a method that prints board in console

---
+++ FlippedTicTacToeInterface/GameConsoleUtils.cs
@@ -0,0 +1,63 @@
+﻿using System;
+using FlippedTicTacToe;
+
+namespace FlippedTicTacToeInterface
+{
+    public class GameConsoleUtils
+    {
+        private const string k_Space = " ";
+        private const string k_ColumnSeparator = "|";
+
+        public static void PrintBoard(eSymbols[,] i_Board)
+        {
+            int numOfRowsIncludeSeparations = i_Board.GetLength(0) * 2;
+
+            printColumnIndexes(i_Board.GetLength(0));
+            for(int rowIndex = 0; rowIndex < numOfRowsIncludeSeparations; rowIndex++)
+            {
+                if(rowIndex % 2 == 0)
+                {
+                    Console.Write($"{rowIndex / 2}{k_ColumnSeparator}");
+                    printSymbolsRow(i_Board, rowIndex / 2);
+                }
+                else
+                {
+                    Console.Write(k_Space);
+                    printSeparationRow(i_Board.GetLength(0));
+                }
+            }
+        }
+
+        private static void printColumnIndexes(int i_Size)
+        {
+            Console.Write(k_Space);
+            for(int colIndex = 0; colIndex < i_Size; colIndex++)
+            {
+                Console.Write($"  {colIndex} ");
+            }
+
+            Console.Write("\n");
+        }
+
+        private static void printSymbolsRow(eSymbols[,] i_Board, int i_RowIndex)
+        {
+            for(int colIndex = 0; colIndex < i_Board.GetLength(0); colIndex++)
+            {
+                char symbol = GameInterface.UserInterfaceConverter.ConvertSymbolToChar(i_Board[i_RowIndex, colIndex]);
+                Console.Write($" {symbol.ToString()} {k_ColumnSeparator}");
+            }
+
+            Console.Write("\n");
+        }
+
+        private static void printSeparationRow(int i_Width)
+        {
+            for (int colIndex = 0; colIndex < i_Width; colIndex++)
+            {
+                Console.Write("====");
+            }
+
+            Console.Write("\n");
+        }
+    }
+}
---
+++ FlippedTicTacToeInterface/Program.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using FlippedTicTacToe;
+
+namespace FlippedTicTacToeInterface
+{
+    public class Program
+    {
+        public static void Main()
+        {
+            GameEngine g = new GameEngine();
+            g.SetFirstPlayer();
+            g.SetSecondPlayer(false);
+            g.SetGameBoardSize(8);
+            g.RestartGame();
+            GameConsoleUtils.PrintBoard(g.GameBoard.Board);
+        }
+    }
+}
